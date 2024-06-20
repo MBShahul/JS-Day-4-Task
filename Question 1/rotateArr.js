4249d2b62909f1e1bff6959970e77e302d01b819
MBShahul <iamshahulms6@gmail.com>
2024-06-20 10:43:30 +0530

    another commit

--- Question 1/rotateArr.js
+++ Question 1/rotateArr.js
@@ -1,4 +1,19 @@
+let numbers = [10,20,30,40,50,60,70];
+let k = 3;
+
 //Anonymous function
+let rotateArray = function(arr, k) {
+    let n = arr.length;
+    k = k % n;
+    return arr.slice(-k).concat(arr.slice(0, -k));
+};
+let rotatedNumbers = rotateArray(numbers, k);
+console.log(rotatedNumbers);
 
 
 //IIFE
+(function(arr, k) {
+    k = k % arr.length;
+    let rotated = arr.slice(-k).concat(arr.slice(0, -k));
+    console.log(rotated); 
+})(numbers, k);
